a924d90f5060b6e16d5f3998925677d551f95a17
Martin Denci <denci.martin@gmail.com>
2016-05-24 06:43:55 +0200

    uprava pro zobrazeni

--- MDUITableViewController.h
+++ MDUITableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MDUITableViewController.h
 //  UITableViewController Challenge Solution.
 //
 //  Created by Martin Denci on 24/05/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MDUITableViewController : UITableViewController
 
 @end
-
